29b29eb927ce424822ce26f40d72143148f7d115
Nurlan18 <99880506+Nurlan18@users.noreply.github.com>
2022-03-15 04:18:06 -0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,11 @@
+a=(int)(input("1-ci ededi daxil edin: "))
+b=(int)(input("2-ci ededi daxil edin: "))
+c=input("Emeli daxil edin: ")
+if(c=='+'):
+    print(a+b)
+elif(c=='-'):
+    print(a-b)
+elif(c=='*'):
+    print(a*b)
+elif(c=='/'):
+    print(a/b)
